af39c1dac77de9aa4301d4fb6a851796d0fea0e2
Eric Hirsch <ericonsi@yahoo.com>
2018-12-12 10:44:40 -0500

    Add project files.

---
+++ England2/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace England2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Thursday()
+        {
+            return View();
+        }
+
+        public ActionResult Friday()
+        {
+            return View();
+        }
+
+
+        public ActionResult Saturday()
+        {
+            return View();
+        }
+
+        public ActionResult Sunday()
+        {
+            return View();
+        }
+
+        public ActionResult Monday()
+        {
+            return View();
+        }
+
+        public ActionResult Tuesday()
+        {
+            return View();
+        }
+    }
+}
---
+++ England2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="England2.MvcApplication" Language="C#" %>
